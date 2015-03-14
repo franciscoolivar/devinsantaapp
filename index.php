cae13e07c32f4f2098c0e46ba91677e4ef2c7888
Anonymous <franciscoolivar@gmail.com>
2015-03-14 14:03:18 +0000

    ajustes na conexao

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
   <body>
+    <h1>Testando a Conexação com Mysql</h1>
     <?php
-      $conecta = mysql_connect("mysql23283-devinsanta.jelastic.websolute.net.br", "root", "4BcxHqxut1") or print (mysql_error()); 
+      $conecta = mysql_connect("mysql23283-devinsanta.jelastic.websolute.net.br", "olivar", "abcd1234") or print (mysql_error()); 
       mysql_select_db("devinsantaapp", $conecta) or print(mysql_error()); 
       $sql = "SELECT * FROM usuario"; 
       $result = mysql_query($sql, $conecta); 
