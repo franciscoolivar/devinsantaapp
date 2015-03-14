4a1fa193a97375bf7a33fbe2bb1514acc394dad5
Anonymous <franciscoolivar@gmail.com>
2015-03-14 13:44:51 +0000

    Alteracoes

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <html>
   <body>
     <?php
-      $conecta = mysql_connect('mysql23283-devinsanta.jelastic.websolute.net.br', 'root', '4BcxHqxut1') or print (mysql_error()); 
+      $conecta = mysql_connect("mysql23283-devinsanta.jelastic.websolute.net.br", "root", "4BcxHqxut1") or print (mysql_error()); 
       mysql_select_db("devinsantaapp", $conecta) or print(mysql_error()); 
       $sql = "SELECT * FROM usuario"; 
       $result = mysql_query($sql, $conecta); 
  
- 
-     while($consulta = mysql_fetch_array($result)) { 
+      while($consulta = mysql_fetch_array($result)) { 
        print "Coluna1: $consulta[coluna1] - Coluna2: $consulta[coluna2]<br>"; 
       } 
      mysql_free_result($result);   
